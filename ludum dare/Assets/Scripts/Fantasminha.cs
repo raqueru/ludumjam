52d62cc2c684bef7f45ba950a20b290dfefda221
Alex Malheiros <alx.malheiros@gmail.com>
2020-10-03 18:05:44 -0300

    Enemy anims

---
+++ ludum dare/Assets/Scripts/Fantasminha.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fantasminha : MonoBehaviour
+{
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            Debug.Log("Yameteee");
+            Destroy(this.gameObject,5);
+        }
+    }
+}
