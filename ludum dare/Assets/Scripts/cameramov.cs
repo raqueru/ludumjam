f5b0763e98cd135344ac3c22b3207931ff4b9082
raks <raquelolho@hotmail.com>
2020-10-02 21:02:30 -0300

    personagem se mexendo pra direita

---
+++ ludum dare/Assets/Scripts/cameramov.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameramov : MonoBehaviour
+{ 
+    public GameObject Alvo;
+    public float moveSpeed;
+    public Vector3 offset;
+    private Transform transfPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transfPlayer = Alvo.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //A: move-se junto com o player 
+        transform.position = Vector3.Lerp(transform.position, transfPlayer.position + new Vector3(0, 0, -10), moveSpeed * Time.deltaTime);
+    }
+}
---
+++ ludum dare/Assets/Scripts/playermov.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermov : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          rb.transform.position = rb.position + Vector2.right * Time.fixedDeltaTime; //movimenta o personagem
+    }
+}
