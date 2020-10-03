b19f32c23f609a81bdd29a1d8fc5d75401462c07
raks <raquelolho@hotmail.com>
2020-10-03 16:25:12 -0300

    mais smooth

--- ludum dare/Assets/Scripts/playermov.cs
+++ ludum dare/Assets/Scripts/playermov.cs
@@ -22,7 +22,7 @@ public class playermov : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
          rb.transform.position = rb.position + Vector2.right * playerspeed * Time.fixedDeltaTime; //movimenta o personagem
@@ -32,7 +32,7 @@ public class playermov : MonoBehaviour
 
 
         }
-        changelayer();
+        StartCoroutine( changelayer());
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -51,30 +51,32 @@ public class playermov : MonoBehaviour
         }
     }
 
-    void changelayer()
+    IEnumerator changelayer()
     {
         float playerboxhalf = GetComponent<BoxCollider2D>().bounds.extents.y;
         Vector3 playerpos = new Vector3(transform.position.x, 0, 0);
-        if (Input.GetKeyDown(KeyCode.UpArrow)&&!foregrounded)
+        if (Input.GetKey(KeyCode.UpArrow)&&!foregrounded)
         {
             float foregroundhalfy = foreground.GetComponent<BoxCollider2D>().bounds.extents.y;
             float foregroundcentery = foreground.GetComponent<BoxCollider2D>().bounds.center.y;
             float foregroundyupper = foregroundcentery + foregroundhalfy;
-            transform.position = new Vector2(transform.position.x,foregroundyupper+ playerboxhalf);
+            Vector2 NewPos = new Vector2(transform.position.x,foregroundyupper+ playerboxhalf);
+            transform.position = Vector2.Lerp(transform.position,NewPos, 10* Time.deltaTime);
+            yield return new WaitForSeconds(0.1f);
             this.gameObject.layer = 8;
             foregrounded = true;
 
         }
 
-        else if (Input.GetKeyDown(KeyCode.DownArrow)&&foregrounded)
+        else if (Input.GetKey(KeyCode.DownArrow)&&foregrounded)
         {
             float groundhalfy = ground.GetComponent<BoxCollider2D>().bounds.extents.y;
-            //get the center
             float groundcentery = ground.GetComponent<BoxCollider2D>().bounds.center.y;
-            //get the up border
             float groundyupper = groundcentery + groundhalfy;
-            transform.position = new Vector2(transform.position.x,groundyupper+playerboxhalf);
+            Vector2 NewPos = new Vector2(transform.position.x,groundyupper+playerboxhalf);
             this.gameObject.layer = 0;
+            yield return new WaitForSeconds(0.1f);
+            transform.position = Vector2.Lerp(transform.position, NewPos,  Time.deltaTime);
             foregrounded = false;
         }
     }
