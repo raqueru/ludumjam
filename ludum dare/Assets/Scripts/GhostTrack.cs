b31987c0b766ccbfc6b47ee6ddab0e538951f033
Alex Malheiros <alx.malheiros@gmail.com>
2020-10-05 14:18:47 -0300

    Stable

---
+++ ludum dare/Assets/Scripts/GhostTrack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostTrack : MonoBehaviour
+{
+    private GameObject Ghost_Track;
+    private GameObject Player;
+    float distancePassed = 10;
+    private void Start()
+    {
+        Player = FindObjectOfType<playermov>().gameObject;
+    }
+    void Update()
+    {
+
+        if (Player.transform.position.x > transform.position.x + distancePassed)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
