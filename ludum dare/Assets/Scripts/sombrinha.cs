1580659d211473694b0c9a1413b4b671f74bd9fa
Alex Malheiros <alx.malheiros@gmail.com>
2020-10-05 03:32:31 -0300

    LaSombra

--- ludum dare/Assets/Scripts/sombrinha.cs
+++ ludum dare/Assets/Scripts/sombrinha.cs
@@ -11,6 +11,9 @@ public class sombrinha : MonoBehaviour
     [SerializeField]
     Vector3 initialDadTransform;
 
+    [SerializeField]
+    Vector3 initialForeTransform;
+
 
     public Vector3 scaleToGo;
     public Vector3 scaleOriginal;
@@ -19,46 +22,47 @@ public class sombrinha : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        myDad = transform.parent.gameObject;
-        initialDadTransform = myDad.transform.position;
         myTransform = GetComponent<Transform>();
-    }
-        // Update is called once per frame
-     void FixedUpdate()
-     {
-        Vector3 dadNewTransform = myDad.transform.position;
+        myDad = transform.parent.gameObject;
 
-        LocalPositionY(dadNewTransform);
+        initialDadTransform = myDad.transform.position;
+        initialForeTransform = new Vector3(initialDadTransform.x, -7.8f - initialDadTransform.y‬, initialDadTransform.z);
+
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 dadNewTransform = myDad.transform.position;
+        Vector3 dadTransformFore = new Vector3(dadNewTransform.x, dadNewTransform.y, dadNewTransform.z);
 
         if (myDad.transform.tag == "Player")
         {
+
+
             if (myDad.layer == 8)
             {
                 StartCoroutine(LerpScaleUp(scaleToGo, .1f));
+                LocalPositionYAnotherLayer(dadTransformFore);
             }
-            else if (myDad.layer == 0)
+            else
             {
                 StartCoroutine(LerpScaleOriginal(scaleOriginal, .1f));
+                LocalPositionY(dadNewTransform);
             }
         }
 
-        if(myDad.transform.tag == "Obstacle")
+        if (myDad.transform.tag == "Obstacle")
         {
-            if(myDad.layer == 8)
+            if (myDad.layer == 8)
             {
                 myTransform.localScale = new Vector3(.4f, .4f, 1);
-            } 
+            }
             else
             {
                 myTransform.localScale = new Vector3(.6f, .6f, 1);
             }
-
         }
-
-
-
-
-     }
+    }
 
     IEnumerator LerpScaleUp(Vector3 targetScale, float duration)
     {
@@ -71,6 +75,7 @@ public class sombrinha : MonoBehaviour
             time += Time.deltaTime;
             yield return null;
         }
+
         myTransform.localScale = targetScale;
     }
 
@@ -85,23 +90,56 @@ public class sombrinha : MonoBehaviour
             time += Time.deltaTime;
             yield return null;
         }
+
         myTransform.localScale = targetScaleDown;
     }
 
     void LocalPositionY(Vector3 dadNewTransform)
     {
-        Vector3 differenceDad = (dadNewTransform - initialDadTransform);
-        float newY = -differenceDad.y * 2;
-
-        if (dadNewTransform.y > initialDadTransform.y + 1)
-        { 
-            myTransform.localPosition = new Vector3(0,newY, 0);
-        }
-        else if (myTransform.localPosition.y >= -2)
         {
-            myTransform.localPosition = new Vector3(0, -2, 0);
-        }
+            Vector3 differenceDad = (dadNewTransform - initialDadTransform);
+            float newY = -differenceDad.y * 2;
 
+            if (myDad.layer == 0)
+            {
+                if (dadNewTransform.y > initialDadTransform.y + 1)
+                {
+                    myTransform.localPosition = new Vector3(0, newY, 0);
+                }
+                else if (myTransform.localPosition.y <= 4)
+                {
+                    myTransform.localPosition = new Vector3(0, -2, 0);
+                }
+            }
+            else
+            {
+                myTransform.localPosition = new Vector3(0, -2, 0);
+            }
+        }
+    }
+
+    void LocalPositionYAnotherLayer(Vector3 dadTransformFore)
+    {
+        {
+            Vector3 differenceDadFore = (dadTransformFore - initialForeTransform);
+            float newYFore = -differenceDadFore.y * 2f;
+
+            if (myDad.layer == 8)
+            {
+                if (dadTransformFore.y > initialForeTransform.y + 1)
+                {
+                    myTransform.localPosition = new Vector3(0, newYFore, 0);
+                }
+                else if (myTransform.localPosition.y <= 4)
+                {
+                    myTransform.localPosition = new Vector3(0, -2, 0);
+                }
+            } 
+            else
+            {
+                myTransform.localPosition = new Vector3(0, -2, 0);
+            }
+        }
     }
 }
 
